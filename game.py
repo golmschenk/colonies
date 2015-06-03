362feb236c5409b03354f81bffceee42d6e28e56
Greg Olmschenk <golmschenk@gmail.com>
2015-06-03 09:00:47 -0400

    Game: Add basic initializer

---
+++ game.py
@@ -0,0 +1,8 @@
+"""Game class stuff."""
+
+class Game:
+    """The class to hold the game data."""
+    def __init__(self):
+        """Game object initilizer."""
+        self.players = []
+        self.board = []
