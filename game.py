53f27f7648d66aca78f79a53526c822de9f2b8fb
Greg Olmschenk <golmschenk@gmail.com>
2015-06-03 09:26:02 -0400

    Game: Board display

--- game.py
+++ game.py
@@ -13,3 +13,15 @@ class Game:
             self.board.append([])  # Add a new row (empty list).
             for y in range(height):
                 self.board[-1].append(None)  # Add a space in the new row (None object).
+
+    def display_board(self):
+        """Displays the game board."""
+        display_string = ""
+        for row in self.board:
+            for unit in row:
+                if unit:  # If position is not empty...
+                    display_string += str(unit.owner.id)  # Show the unit's owner id.
+                else:
+                    display_string += '.'
+            display_string += '\n'
+        print(display_string)
