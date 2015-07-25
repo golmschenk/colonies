680872d71d516451774c6d05082b8053ef11a49b
Jusctsch5 <schumjs@gmail.com>
2015-07-24 21:37:06 -0500

    Add ability to capture and move Piece objects

--- source/game/piece.py
+++ source/game/piece.py
@@ -21,3 +21,28 @@ class Piece:
         self.width = width
         self.height = height
         self.type = piece_type
+        self.capture_count = 0
+        self.move_count = 0
+
+    def capture(self, player):
+        """
+        Used for passed Player to gain control of Piece
+
+        :param player: The player to gain control of this Piece.
+        :type player: Player
+        """
+        self.player = player
+        self.capture_count = self.capture_count + 1
+
+    def move(self, x, y):
+        """
+        Relocates the passed piece on the gameboard.
+
+        :param x: The units of width to move this piece.
+        :type x: int
+        :param y: The units of height to move this piece.
+        :type y: int
+        """
+        self.width = self.width + x
+        self.height = self.width + y
+        self.move_count = self.move_count + 1
