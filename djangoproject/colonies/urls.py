30613ba2850eed3e263cea8e3419953f1f925391
Greg Olmschenk <golmschenk@gmail.com>
2015-12-01 21:53:33 -0500

    Views: Add basic homepage routing

--- djangoproject/colonies/urls.py
+++ djangoproject/colonies/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from interface import urls as interface_urls
+from interface.views import HomeView
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HomeView.as_view()),
 ]
