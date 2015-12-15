4f75c2808958d705a451132ecfb8164e70da439b
Greg Olmschenk <golmschenk@gmail.com>
2015-12-15 10:45:25 -0500

    Interface: Add a board rows property to the game model

--- djangoproject/interface/models.py
+++ djangoproject/interface/models.py
@@ -1,6 +1,8 @@
 """
 The models file for the interface app.
 """
+import pickle
+
 from django.db import models
 
 
@@ -9,3 +11,14 @@ class Game(models.Model):
     The database model to hold game information and data.
     """
     data = models.BinaryField(default=None)
+
+    @property
+    def board_rows(self):
+        """
+        Retrieves the board rows from the pickled game data.
+
+        :return: The list of board rows
+        :rtype: list[str]
+        """
+        unpickled_data = pickle.loads(self.data)
+        return unpickled_data.board.splitlines()
--- djangoproject/interface/tests/test_models.py
+++ djangoproject/interface/tests/test_models.py
@@ -2,6 +2,8 @@
 The view functions for the interface app.
 """
 import pickle
+from unittest.mock import Mock, patch
+
 from django.test import TestCase
 
 from interface.models import Game
@@ -17,3 +19,14 @@ class TestGameModel(TestCase):
         saved_game = Game.objects.first()
 
         assert pickle.loads(saved_game.data) == data
+
+    @patch('interface.models.pickle.loads')
+    def test_can_retrieve_the_game_board_from_game_data(self, mock_loads):
+        game = Game()
+        game_data = Mock()
+        game_data.board = '1..\n...\n..2'
+        mock_loads.return_value = game_data
+
+        board_rows = game.board_rows
+
+        assert board_rows == ['1..', '...', '..2']
