2778963b2fb1d55e38716206fde4d63da9cfe178
Greg Olmschenk <golmschenk@gmail.com>
2015-12-15 10:26:09 -0500

    Interface: Pass game board context to template

--- djangoproject/interface/tests/test_views.py
+++ djangoproject/interface/tests/test_views.py
@@ -9,7 +9,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.test import TestCase
 
-from interface.views import NewGameView
+from interface.views import NewGameView, GameView
 
 
 class TestHomePage(TestCase):
@@ -38,19 +38,40 @@ class TestNewGamePage(TestCase):
 
 
 class TestGamePage(TestCase):
-    def test_game_page_renders_game_template(self):
+    @patch('interface.views.get_object_or_404')
+    def test_game_page_renders_game_template(self, mock_get_object_or_404):
         response = self.client.get(reverse('game', kwargs={'game_id': 2}))
+
         self.assertTemplateUsed(response, 'game.html')
 
     def test_template_has_no_table_cells_when_there_is_no_board_argument(self):
         response = render(HttpRequest(), 'game.html')
+
         soup = BeautifulSoup(response.content, 'html.parser')
         board_table = soup.find(id='board_table')
+
         assert 'td' not in str(board_table)
 
     def test_template_has_table_cells_when_there_is_a_board_argument(self):
         board_rows = ['1.2']
+
         response = render(HttpRequest(), 'game.html', context={'board_rows': board_rows})
         soup = BeautifulSoup(response.content, 'html.parser')
         board_table = soup.find(id='board_table')
+
         assert 'td' in str(board_table)
+
+    @patch('interface.views.get_object_or_404')
+    @patch('interface.views.Game')
+    def test_game_id_is_retrieve_by_passed_id_and_set_in_context(self, mock_game_class, mock_get_object_or_404):
+        game_view = GameView()
+        mock_game = Mock()
+        mock_game.board_rows = ['1.2']
+        mock_get_object_or_404.return_value = mock_game
+
+        context = game_view.get_context_data(**{'game_id': 2})
+
+        assert mock_get_object_or_404.called
+        assert mock_get_object_or_404.call_args == ((mock_game_class,), {'pk': 2})
+        assert context['board_rows'] == mock_game.board_rows
+
--- djangoproject/interface/views.py
+++ djangoproject/interface/views.py
@@ -3,6 +3,7 @@ The interface's view functions.
 """
 from django.views.generic import TemplateView, RedirectView
 from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
 
 from interface.models import Game
 
@@ -14,13 +15,6 @@ class HomeView(TemplateView):
     template_name = 'home.html'
 
 
-class GameView(TemplateView):
-    """
-    The game view class. For displaying the game interface page.
-    """
-    template_name = 'game.html'
-
-
 class NewGameView(RedirectView):
     """
     The redirect view to create a new game.
@@ -28,8 +22,28 @@ class NewGameView(RedirectView):
     def get_redirect_url(self):
         """
         Creates the new game an redirects to the game view with the new game's ID passed.
+
         :return: The redirect url of the new game.
         :rtype: str
         """
         game = Game.objects.create()
         return reverse('game', kwargs={'game_id': game.id})
+
+
+class GameView(TemplateView):
+    """
+    The game view class. For displaying the game interface page.
+    """
+    template_name = 'game.html'
+
+    def get_context_data(self, game_id):
+        """
+        Prepares the game data to pass as the context to the template.
+
+        :param game_id: The game to retrieve.
+        :type game_id: Game
+        :return: The context of the template.
+        :rtype: dict
+        """
+        game = get_object_or_404(Game, pk=game_id)
+        return {'board_rows': game.board_rows}
